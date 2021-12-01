04a12bfebd7e1942c178a3885822843d90dbb97a
Antonio Gonzalez Restrepo <angonzalez@sistecredito.com>
2021-12-01 08:53:58 -0500

    Initial structure

---
+++ AR.BackgroundJobs/Startup.cs
@@ -0,0 +1,64 @@
+using AR.BackgroundJobs.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AR.BackgroundJobs
+{
+    public class Startup
+    {
+        private readonly ILogger<Startup> _logger;
+
+        public Startup(IConfiguration configuration, ILogger<Startup> logger)
+        {
+            Configuration = configuration;
+            _logger = logger;
+        }
+
+        public IConfiguration Configuration { get; }
+
+       
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServices();
+
+            #region Startup Log
+            _logger.LogInformation("============ STARTUP ================");
+            _logger.LogInformation($"SQL ConnectionString: {"Hola"}");
+            _logger.LogInformation($"Date = {DateTime.UtcNow.ToLocalTime()}");
+            _logger.LogInformation("============ END STARTUP ============");
+            #endregion Startup Log
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapRazorPages();
+            });
+        }
+    }
+}
